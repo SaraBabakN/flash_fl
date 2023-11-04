40c46188445f6d3d84ea8ff491e243f7fae29e9a
Sara Babakniya <54650395+SaraBabakN@users.noreply.github.com>
2023-11-03 18:39:40 -0700

    Update constants.py

--- constants.py
+++ constants.py
@@ -10,6 +10,4 @@ CIFAR10 = 'cifar10'
 CIFAR100 = 'cifar100'
 TINY = 'tinyimagenet'
 RESNET18 = 'resnet18'
-true_false = [TRUE, FALSE]
-experiment_choices = [NST, DENSE, PDST, SPDST, JMWST]
 model_choices = [RESNET18, 'lenet', 'mnistnet', 'cnn', 'resnet18_tiny', 'resnet34']
